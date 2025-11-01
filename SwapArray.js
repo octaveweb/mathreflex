29ba52c3f8b68a3e80484b7dcb90dc9683f10ded
octaveweb <karansproject955@gmail.com>
2025-11-01 11:07:43 +0530

    Swap Array Function

---
+++ SwapArray.js
@@ -0,0 +1,21 @@
+function rainxArray(a) {
+  function indexInd(n) {
+    let j = Math.floor(Math.random() * 3) + 1;
+    return j;
+  }
+
+
+  let arr = a;
+  let rn = indexInd();
+  console.log(rn);
+
+
+
+  let temp;
+  temp = arr[0];
+  arr[0] = arr[rn];
+  arr[rn] = temp
+}
+
+// let arrry = [5, 0, 0, 0]
+// rainxArray(arrry)
