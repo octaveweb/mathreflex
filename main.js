3ec9522c78ef1748c5a4b870842ab672b8678e04
octaveweb <karansproject955@gmail.com>
2025-11-13 23:46:44 +0530

    add game over html section

--- main.js
+++ main.js
@@ -1,61 +1,17 @@
 import qustion from './js/qustion.model.js';
+
 let qsNum = 1;
 let currectAns = 0;
-
+let totalQustion = 1;
 function colorAddRightWrong(id, backcolor, bordercolor) {
     const id_target = document.getElementById(`${id}`);
-
     id_target.style.boxShadow = `inset 0px 0px 23px 4px ${backcolor}`;
     id_target.style.border = bordercolor;
 
     setTimeout(() => {
         id_target.style.boxShadow = `inset 0px 0px 23px 4px none`;
-        id_target.style.border = ` border: 2px solid none`;
-
+        id_target.style.border = `2px solid transparent`;
     }, 1000);
-
-
-}
-function next(val) {
-    let next = document.getElementById("next");
-    next.style.zIndex = "888";
-    let nextQustion = qustion()
-    let { qustions, ans, optionArrNwe } = nextQustion;
-
-    if (val) {
-        setTimeout(() => {
-            countCorrectAns()
-            // Total correct 
-            insertInDom("totalCorect", currectAns)
-            // New qustion 
-            countQustion();
-            insertInDom("qustion", qustions);
-
-
-            buttonAllDOM(optionArrNwe, ans)
-            console.log(ans);
-
-            next.style.zIndex = "-1";
-            colorAddRightWrong("qustion-container", "transparent")
-
-        }, 1000);
-    } else {
-        setTimeout(() => {
-            countQustion();
-            // Total correct 
-            insertInDom("totalCorect", currectAns)
-            // New Qustion 
-            insertInDom("qustion", qustions);
-
-            buttonAllDOM(optionArrNwe, ans)
-            console.log(ans);
-
-            next.style.zIndex = "-1";
-            colorAddRightWrong("qustion-container", "transparent")
-            colorAddRightWrong("qustion-container", "transparent")
-        }, 1000);
-    }
-
 }
 
 function insertInDom(parent, content) {
@@ -66,6 +22,7 @@ function countQustion() {
     qsNum = qsNum + 1;
     insertInDom("qustion_no", qsNum);
 }
+
 function countCorrectAns() {
     currectAns = currectAns + 1;
     insertInDom("totalCorect", currectAns);
@@ -81,13 +38,8 @@ function buttonAllDOM(arr, ans) {
 
     buttons.forEach((btn, i) => {
         btn.innerText = arr[i];
-
         btn.onclick = () => {
-            console.log(`current qustion number is: ${qsNum}`);
-
-            if (btn.disabled) return; // prevent spam clicking
-
-            // Disable all buttons temporarily
+            if (btn.disabled) return;
             buttons.forEach(b => b.disabled = true);
 
             setTimeout(() => {
@@ -95,48 +47,88 @@ function buttonAllDOM(arr, ans) {
                     colorAddRightWrong("qustion-container", "#3da30082");
                     colorAddRightWrong(btn.id, "transparent", "1px solid #3da30082");
                     next(1);
-                    
                 } else {
                     colorAddRightWrong("qustion-container", "#ff000052");
                     colorAddRightWrong(btn.id, "transparent", "1px solid #ff000052");
                     next(0);
                 }
 
-                // Re-enable buttons after delay
                 setTimeout(() => {
                     buttons.forEach(b => b.disabled = false);
                 }, 1000);
-
             }, 1000);
 
             setTimeout(() => {
                 colorAddRightWrong("qustion-container", "transparent");
                 colorAddRightWrong(btn.id, "transparent", "1px solid transparent");
-                console.log(btn.id);
             }, 2000);
-
-
         };
     });
 }
 
+function next(val) {
+    let next = document.getElementById("next");
+    next.style.zIndex = "888";
+    let nextQustion = qustion();
+    let { qustions, ans, optionArrNwe } = nextQustion;
 
+    if (val) {
+        setTimeout(() => {
+            countCorrectAns();
+            insertInDom("totalCorect", currectAns);
+            countQustion();
+            insertInDom("qustion", qustions);
+            buttonAllDOM(optionArrNwe, ans);
+            next.style.zIndex = "-1";
+            colorAddRightWrong("qustion-container", "transparent");
+            endgame(totalQustion); // total number of questions
+        }, 1000);
+    } else {
+        setTimeout(() => {
+            countQustion();
+            insertInDom("totalCorect", currectAns);
+            insertInDom("qustion", qustions);
+            buttonAllDOM(optionArrNwe, ans);
+            next.style.zIndex = "-1";
+            colorAddRightWrong("qustion-container", "transparent");
+            endgame(totalQustion);
+        }, 1000);
+    }
+}
+
+function endgame(totalQustions) {
+
+    let gameSection = document.querySelector("#game-paly")
+    let gameOver = document.querySelector("#gameOver")
+
+ 
+
+    if (qsNum > totalQustions) {
+           gameSection.style.display = "none";
+    gameOver.style.display = "flex";
+    document.querySelector(".message").style.display = "none"
+        gameOver.innerHTML = `
+          <div class="gritBox">
+                <div class="content">
+                    <h3>Wonderful, <br> #name</h3>
+                    
+                    <p>You get <span class="scoreBadge"><span class="tinyDot" aria-hidden="true"></span>${currectAns}/${totalQustion}</span></p>
+                </div>
+                <button id="restartGame" type="button">Replay</button>
+            </div>
+        `;
+
+        
+    }
+}
 
 function main() {
     let q = qustion();
     let { qustions, ans, optionArrNwe } = q;
 
-    // qustion
-    insertInDom("qustion", qustions)
-    // botton
-    buttonAllDOM(optionArrNwe, ans)
-    console.log(ans);
-
-    insertInDom("messageData", "Welcome")
-
-
-
+    insertInDom("qustion", qustions);
+    buttonAllDOM(optionArrNwe, ans);
+    insertInDom("messageData", "Welcome");
 }
-main()
 
-
+main();
